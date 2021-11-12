2e3b6d40976020c37cee5252dcf7d2fdc78f260d
Yusuf ARSLAN <arslanon@outlook.com>
2021-11-12 21:23:29 +0300

    random cat images

--- 03 - RandomAnimals/script.js
+++ 03 - RandomAnimals/script.js
@@ -1,19 +1,31 @@
 // elements
-const dog = document.querySelector("#dog");
-const btnRandomDog = document.querySelector("#btnRandomDog");
-
+const dog = document.querySelector("#dog")
+const btnRandomDog = document.querySelector("#btnRandomDog")
+const cat = document.querySelector("#cat")
+const btnRandomCat = document.querySelector("#btnRandomCat")
 // events
-btnRandomDog.addEventListener("click", getRandomDog);
+btnRandomDog.addEventListener("click", getRandomDog)
+btnRandomCat.addEventListener("click", getRandomCat)
 
+// functions
 function getRandomDog() {
   fetch("https://random.dog/woof.json")
     .then((res) => res.json())
     .then((data) => {
       if (data.url.includes('.mp4')) {
-        getRandomDog();
+        getRandomDog()
       } else {
         dog.innerHTML = `
-            <img src="${data.url}" class="img-fluid"/>`;
+            <img src="${data.url}" class="img-fluid"/>`
       }
-    });
+    })
+}
+
+function getRandomCat(){
+    fetch("https://aws.random.cat/meow")
+      .then(res => res.json())
+      .then(data => {
+          cat.innerHTML = `
+          <img src="${data.file}" class="img-fluid"/>`
+      })
 }
