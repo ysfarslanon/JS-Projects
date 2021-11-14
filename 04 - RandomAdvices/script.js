2f60e5af0f9c75b65d6ad6d032560985b0aac36a
Yusuf ARSLAN <arslanon@outlook.com>
2021-11-14 17:47:15 +0300

    add btn and styles

--- 04 - RandomAdvices/script.js
+++ 04 - RandomAdvices/script.js
@@ -2,7 +2,6 @@ let adviceText = document.getElementById("adviceText")
 getAdvice()
 
 function getAdvice() {
-
      fetch("https://api.adviceslip.com/advice")
     .then(res=>res.json())
     .then(data => {
