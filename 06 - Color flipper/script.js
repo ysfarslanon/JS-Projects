b891fa04bcec9282cea5c6e8987af35666a4dfa8
Yusuf ARSLAN <arslanon@outlook.com>
2021-11-20 21:21:53 +0300

    rgb color

--- 06 - Color flipper/script.js
+++ 06 - Color flipper/script.js
@@ -1,21 +1,40 @@
 const btnHex = document.getElementById("btnHex");
+const btnRgb = document.getElementById("btnRgb");
 const colors=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F',];
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * colors.length)
+function getRandomNumber(limit) {
+    return Math.floor(Math.random() * limit)
 }
 
 function createHexColor() {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
-        hexColor += colors[getRandomNumber()];
+        hexColor += colors[getRandomNumber(colors.length)];
     }
     return hexColor
 }
 
-function applyColor() {
+function applyHexColor() {
     document.body.style.backgroundColor = createHexColor()
     document.getElementById("colorText").innerHTML = createHexColor()
 }
 
-btnHex.addEventListener("click",applyColor)
+btnHex.addEventListener("click",applyHexColor)
+
+function createRgbColor() {
+    let rgbColor = "RGB("
+    for (let i = 0; i < 3; i++) {
+        if(i===2){
+            rgbColor += `${(getRandomNumber(256)).toString()})`
+        }else{
+            rgbColor += `${(getRandomNumber(256)).toString()},`
+        }
+    }
+    return rgbColor
+}
+
+function applyRgbColor() {
+    document.body.style.backgroundColor = createRgbColor()
+    document.getElementById("colorText").innerHTML = createRgbColor()
+}
+btnRgb.addEventListener("click",applyRgbColor)
