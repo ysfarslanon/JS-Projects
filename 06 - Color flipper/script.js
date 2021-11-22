5157a27dff6966d50dcd6b88273d55fe083f2c39
Yusuf ARSLAN <arslanon@outlook.com>
2021-11-22 18:45:41 +0300

    small refactor

--- 06 - Color flipper/script.js
+++ 06 - Color flipper/script.js
@@ -2,7 +2,7 @@ const btnHex = document.getElementById("btnHex");
 const btnRgb = document.getElementById("btnRgb");
 const btnCopy = document.getElementById("btnCopy");
 const colorText = document.getElementById("colorText");
-const colors=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F',];
+const colors = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
 
 function getRandomNumber(limit) {
     return Math.floor(Math.random() * limit)
@@ -21,7 +21,7 @@ function applyHexColor() {
     colorText.innerHTML = createHexColor()
 }
 
-btnHex.addEventListener("click",applyHexColor)
+btnHex.addEventListener("click", applyHexColor)
 
 function createRgbColor() {
     let rgbColor = "RGB("
@@ -39,10 +39,10 @@ function applyRgbColor() {
     document.body.style.backgroundColor = createRgbColor()
     colorText.innerHTML = createRgbColor()
 }
-btnRgb.addEventListener("click",applyRgbColor)
+btnRgb.addEventListener("click", applyRgbColor)
 
 function copy() {
     const cb = navigator.clipboard
     cb.writeText(colorText.innerHTML).then(()=> alert(`Copied: ${colorText.innerHTML}`))
 }
-btnCopy.addEventListener("click",copy)
+btnCopy.addEventListener("click", copy)
